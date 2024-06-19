2fa096ef9793a1e4f44b19f7a117e0048b8c1f9c
quang.pham <quang.pham@s3corp.com.vn>
2024-06-19 16:16:57 +0700

    init commit

---
+++ src/components/project.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionHeading from "@/components/section-heading";
+
+function Project() {
+    return (
+        <section>
+            <SectionHeading>My projects</SectionHeading>
+            
+        </section>
+    );
+}
+
+export default Project;
