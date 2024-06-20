9431ab6cc889aa1f75d46bb1de26295284f436ad
QuangPham789 <nhutquang23@gmail.com>
2024-06-20 21:48:30 +0700

    update header

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,16 +1,28 @@
 'use client'
-import React from 'react';
+import React, {useEffect} from 'react';
 import SectionHeading from "@/components/section-heading";
 import {motion} from 'framer-motion';
+import {useInView} from "react-intersection-observer";
+import {useActiveSectionContext} from "@/context/active-section-context";
 
 function About() {
+    const {ref, inView} = useInView()
+    const {setActiveSection} = useActiveSectionContext()
+    useEffect(() => {
+        if (inView) {
+            setActiveSection("About")
+        }
+    }, [inView, setActiveSection])
+
     return (
         <motion.section
+            ref={ref}
             id="about"
             className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
             initial={{opacity: 0, y: 100}}
             animate={{opacity: 1, y: 0}}
             transition={{delay: 0.175}}
+
         >
             <SectionHeading>About me</SectionHeading>
             <p className="mb-3">
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+'use client'
+import React, {useEffect} from 'react';
 import SectionHeading from "@/components/section-heading";
 import {projectsData} from "@/lib/data";
 import Project from "@/components/project";
+import {useInView} from "react-intersection-observer";
+import {useActiveSectionContext} from "@/context/active-section-context";
 
 function Projects() {
+    const {ref, inView} = useInView()
+    const {setActiveSection} = useActiveSectionContext()
+    useEffect(() => {
+        if (inView) {
+            setActiveSection("Projects")
+        }
+    }, [inView, setActiveSection])
     return (
-        <section id="projects" className="scroll-mt-28">
+        <section ref={ref} id="projects" className="scroll-mt-28">
             <SectionHeading>My projects</SectionHeading>
             <div>
                 {
